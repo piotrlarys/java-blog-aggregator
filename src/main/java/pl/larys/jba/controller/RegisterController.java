69d2d91401fc008bb7f4806a08d20e6306ef202f
Piotr <piotr.larys@gmail.com>
2016-05-18 23:19:19 +0200

    complete aplication

--- src/main/java/pl/larys/jba/controller/RegisterController.java
+++ src/main/java/pl/larys/jba/controller/RegisterController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.larys.jba.entity.User;
 import pl.larys.jba.service.UserService;
 
@@ -30,12 +31,13 @@ public class RegisterController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result) {
+    public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result, RedirectAttributes redirectAttributes) {
         if (result.hasErrors()) {
             return "user-register";
         }
         userService.save(user);
-        return "redirect:/register.html?success=true";
+        redirectAttributes.addFlashAttribute("success", true);
+        return "redirect:/register.html";
     }
 
     @RequestMapping("/available")
